9fff5ba717807087a46dfe74f612c5637a50de03
Jiahao Zhang <jiahaozhang@modv-ve507-0100.apn.wlan.upenn.edu>
2017-11-15 07:50:38 -0500

    second

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "ndarrays.h"
 int main(){
-	ndarrays a(3,1,2,3);
+	ndarrays a(3,2,1,1);
+	a(0,0,0)=2;
+	a(1,0,0)=2;
 	std::cout<<a;
 }
--- ndarrays.h
+++ ndarrays.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <new>
 #include <iostream>
+#ifndef __NDARRAYS_H_INCLUDED__
+#define __NDARRAYS_H_INCLUDED__
 class ndarrays{
 public:
 //construct differnt dimention list//
@@ -19,84 +21,4 @@ private:
 	double *p;
 	int linearlength;
 };
-
-ndarrays::ndarrays(int dimen,...){
-	/*construct a ndimentional array, the first argument is it's dimention, the comming arguments are the lengths of different dimentions*/
-	va_list args;
-	std::vector<int> a(0);
-	va_start(args,dimen);
-	dimention=dimen;
-	int temp=1;
-	int temp2;
-	for(int i=0;i<dimen;i++){
-		temp2=va_arg(args,int);
-		a.push_back(temp2);
-		temp=temp*(temp2);
-	}
-	va_end(args);
-	size=a;
-	linearlength=temp;
-	p=new double[temp];
-}
-ndarrays::ndarrays(const ndarrays& a){
-	size=a.size;
-	dimention=a.dimention;
-	int temp=1;
-	for(auto i=size.begin();i<size.end();i++){
-		temp=temp*(*i);
-	}
-	p=new double[temp];
-	for(size_t t=0;t<a.linearlength;t++){
-		p[t]=a.p[t];
-	}
-}
-ndarrays& ndarrays::operator=(const ndarrays& a){
-	size=a.size;
-	dimention=a.dimention;
-	int temp=1;
-	for(auto i=size.begin();i<size.end();i++){
-		temp=temp*(*i);
-	}
-	p=new double[temp];
-	for(size_t t=0;t<a.linearlength;t++){
-		p[t]=a.p[t];
-	}
-	return *this;
-}
-double& ndarrays::operator()(int a,...){
-	int temp;
-	va_list arg;
-	int first;
-	va_start(arg,a);
-	first=a;
-	if(dimention ==1){
-		return p[first];
-	}
-	std::vector<int> coord(0);
-	coord.push_back(first);
-	for(size_t t=1;t<dimention;t++){
-		first=va_arg(arg,int);
-		coord.push_back(first);
-	}
-	va_end(arg);
-	for(size_t t=0;t<dimention-1;t++){
-		if(t==0) temp=coord[t]*size[t+1]+coord[t+1];
-		else temp=temp*size[t+1]+coord[t+1];
-	}
-	return p[temp];
-}
-std::ostream& operator<<(std::ostream& os, ndarrays& out){
-	int temp=1;
-	os<<"the dimention of the array is: "<<out.dimention<<std::endl;
-	os<<"the sizes of different dimention are:"<<std::endl;
-	for(auto t=out.size.cbegin();t!=out.size.cend();t++){
-		os<<*t<<" ";
-		temp=(*t)*temp;
-	}
-	os<<std::endl;
-	os<<"elements are:"<<std::endl;
-	for(size_t t=0;t<temp;t++){
-		os<<(out.p)[t]<<std::endl;
-	}
-	return os;
-}
+#endif
